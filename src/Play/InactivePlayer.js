ec083564a66d46792c1ea20d4d92984b07084dde
Richard Zhang <zkf609@gmail.com>
2021-01-04 16:43:53 -0500

    add Inactive Player

---
+++ src/Play/InactivePlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InactivePlayer = props => {
+
+
+
+    return (
+        <div className='player'>PLAYER Inactive</div>
+    );
+}
+
+export default InactivePlayer;
