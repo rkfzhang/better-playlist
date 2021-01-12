6f4812d068eaf53b5cb6b904e80c5afcb94d4ee2
Richard Zhang <zkf609@gmail.com>
2021-01-11 19:00:04 -0500

    Update PlaylistHeader.js

--- src/Collection/PlaylistHeader.js
+++ src/Collection/PlaylistHeader.js
@@ -13,7 +13,7 @@ const PlaylistHeader = props => {
         <div className='playlist-header'>
             <h1>{props.selectedPlaylist.name}</h1>
             <Button onClick={() => play(props.selectedPlaylist)} variant="success" >Play</Button>
-            <p><small>Please wait ~30s before hitting play to compile playlist</small></p>
+            <p><small>Please wait ~30s before hitting play for app to compile playlist and identify any changes made</small></p>
         </div>
     );
 }
